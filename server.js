b9ac700d2c26a7feeba54a9aa77e466de53b7409
asquaredigital <55409584+asquaredigital@users.noreply.github.com>
2021-04-17 18:00:24 +0530

    commit6

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/herokudemo21'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/herokudemo21/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
